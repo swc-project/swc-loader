4c617541128e5e50a3c04b0d2d063057eca49f47
강동윤 <kdy1@outlook.kr>
2019-01-08 13:19:25 +0900

    implement basical loader

---
+++ README.md
@@ -0,0 +1,3 @@
+# swc-loader
+
+
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const swc = require('swc');
+
+function makeLoader() {
+    return function (source, inputSourceMap) {
+        // Make the loader async
+        const callback = this.async();
+
+        callback(null, transform(source).code);
+    };
+}
+
+module.exports = makeLoader();
