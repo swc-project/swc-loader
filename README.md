2e815759b048c51d61d88017cd70de2d9d302d30
강동윤 <kdy1@outlook.kr>
2019-01-16 13:37:59 +0900

    implement swc-loader

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This package allows transpiling JavaScript files using swc and webpack.
 
 ## Installation
 ```sh
-npm install -D swc-loader webpack
+npm i --save-dev swc-loader webpack
 ```
 
 
@@ -21,6 +21,31 @@ module: {
       }
     }
   ]
+}
+```
 
+
+You can pass options to the loader by using the option property.
+
+```js
+
+module: {
+  rules: [
+    {
+      test: /\.ts$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'swc-loader',
+        options: {
+          jsc: {
+            parser: {
+              syntax: "typescript"
+            }
+          }
+        }
+      }
+    }
+  ]
+}
 
 ```
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const loaderUtils = require('loader-utils');
 const swc = require('swc');
 
 function makeLoader() {
@@ -5,7 +6,61 @@ function makeLoader() {
         // Make the loader async
         const callback = this.async();
 
-        callback(null, transform(source).code);
+        let loaderOptions = loaderUtils.getOptions(this) || {};
+
+        // Standardize on 'sourceMaps' as the key passed through to Webpack, so that
+        // users may safely use either one alongside our default use of
+        // 'this.sourceMap' below without getting error about conflicting aliases.
+        if (
+            Object.prototype.hasOwnProperty.call(loaderOptions, "sourceMap") &&
+            !Object.prototype.hasOwnProperty.call(loaderOptions, "sourceMaps")
+        ) {
+            loaderOptions = Object.assign({}, loaderOptions, {
+                sourceMaps: loaderOptions.sourceMap,
+            });
+            delete loaderOptions.sourceMap;
+        }
+
+
+        const programmaticOptions = Object.assign({}, loaderOptions, {
+            filename,
+            inputSourceMap: inputSourceMap || undefined,
+
+            // Set the default sourcemap behavior based on Webpack's mapping flag,
+            // but allow users to override if they want.
+            sourceMaps:
+                loaderOptions.sourceMaps === undefined
+                    ? this.sourceMap
+                    : loaderOptions.sourceMaps,
+
+            // Ensure that Webpack will get a full absolute path in the sourcemap
+            // so that it can properly map the module back to its internal cached
+            // modules.
+            sourceFileName: filename,
+        });
+        // Remove loader related options
+        delete programmaticOptions.customize;
+        delete programmaticOptions.cacheDirectory;
+        delete programmaticOptions.cacheIdentifier;
+        delete programmaticOptions.cacheCompression;
+        delete programmaticOptions.metadataSubscribers;
+
+        if (programmaticOptions.sourceMaps === "inline") {
+            // Babel has this weird behavior where if you set "inline", we
+            // inline the sourcemap, and set 'result.map = null'. This results
+            // in bad behavior from Babel since the maps get put into the code,
+            // which Webpack does not expect, and because the map we return to
+            // Webpack is null, which is also bad. To avoid that, we override the
+            // behavior here so "inline" just behaves like 'true'.
+            programmaticOptions.sourceMaps = true;
+        }
+
+        try {
+            const output = swc.transformSync(source, programmaticOptions);
+            callback(null, [output.code, output.map])
+        } catch (e) {
+            callback(e)
+        }
     };
 }
 
