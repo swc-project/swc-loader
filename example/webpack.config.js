633965406ea6595fba95155ed5f028096c5980e8
Fábio Santos <fabiosantosart@gmail.com>
2019-01-19 16:39:56 +0000

    add example

---
+++ example/webpack.config.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'index.js'),
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: require.resolve('..'),  // you would put swc-loader
+          options: {
+            jsc: {
+              transform: {
+                react: {
+                  pragma: 'React.createElement',
+                  pragmaFrag: 'React.Fragment',
+                  throwIfNamespace: true,
+                  development: false,
+                  useBuiltins: false
+                }
+              }
+            }
+          }
+        }
+      }
+    ]
+  }
+}
