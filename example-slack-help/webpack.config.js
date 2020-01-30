d8e4f8fb2c6d88b8936e96ac71574bc662534410
강동윤 <kdy1997.dev@gmail.com>
2020-01-30 14:20:42 +0900

    WIP: help

---
+++ example-slack-help/webpack.config.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: require.resolve(".."), // you would put swc-loader
+                    options: {
+                        jsc: {
+                            target: "es2015",
+                            parser: {
+                                syntax: "ecmascript",
+                                jsx: true
+                            },
+                            transform: {
+                                react: {
+                                    pragma: "React.createElement",
+                                    pragmaFrag: "React.Fragment",
+                                    throwIfNamespace: true,
+                                    development: false,
+                                    useBuiltins: false
+                                }
+                            }
+                        },
+                        module: {
+                            type: "commonjs"
+                        }
+                    }
+                }
+            }
+        ]
+    }
+};
