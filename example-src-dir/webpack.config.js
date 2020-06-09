043170c8377a8527ca1e7f11b063a42f83c184aa
강동윤 <kdy1997.dev@gmail.com>
2020-06-09 19:12:01 +0900

    source map

--- example-src-dir/webpack.config.js
+++ example-src-dir/webpack.config.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 module.exports = {
     mode: "development",
+    devtool: 'source-map',
     entry: path.join(__dirname, "src", "index.js"),
     output: {
         path: path.join(__dirname, "build"),
